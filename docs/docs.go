0e86e6f576ee512f548d1e41255755e1578a1b03
Shokhboz Abdullaev <shokhboz11abdullayev@gmail.com>
2025-10-14 16:54:14 +0500

    Fix Swagger host for Render

--- docs/docs.go
+++ docs/docs.go
@@ -644,7 +644,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "portfolio-backend-3o6v.onrender.com",
+	Host:             "https://portfolio-backend-3o6v.onrender.com",
 	BasePath:         "/",
 	Schemes:          []string{"https"},
 	Title:            "Portfolio API",
